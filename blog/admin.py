983a65ad017bd49978d1e2a8451674c4e187be33
thomjr5 <joshuarthompson@hotmail.com>
2016-06-06 05:05:33 +1200

    Added Draft, Security and Comments features

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title','author','created_date','published_date']
     
 admin.site.register(Post, PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author','post','created_date','approved_comment']
+
+
+admin.site.register(Comment,CommentAdmin)
