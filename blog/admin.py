f41ce0d3f57d9001fbe252d29ec5582ab3db27ff
thomjr5 <joshuarthompson@hotmail.com>
2016-06-05 22:52:32 +1200

    created URLs, views and templates for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','created_date','published_date']
+    
+admin.site.register(Post, PostAdmin)
